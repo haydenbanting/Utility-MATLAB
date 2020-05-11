d71434328e5e04a97586f03a31e0a20def9ec812
Hayden Banting <hayden.banting@gmail.com>
2020-05-11 12:20:26 -0400

    Updated csv to mat function documentation

--- csvToMat.m
+++ csvToMat.m
@@ -1,8 +1,11 @@
 function M = csvToMat(filename, numCols, numHeaderLines)
 %-------------------------------------------------------------------------%
-% This function converts data stored in a csv file into a matrix. Header
-% (labels) lines at top of file are ignored and only numeric data is
-% stored. 
+% Author: Hayden Banting
+% Version: 11 May 2020
+%
+% This function converts numerical data stored in a csv file into a
+% matrx. Header lines (data labels) are ignored. Each column should contain
+% the same number of data points.
 %
 % Parameters
 %   filename - path to csv file [string]
@@ -10,12 +13,16 @@ function M = csvToMat(filename, numCols, numHeaderLines)
 %   numHeaderLines - number of header lines at top of file [int]
 %
 % Returns
-%   M - matrix containing numeric data from text file [NxM]
+%   M - matrix containing numeric data from csv file [NxM]
+%
+% Example use for a file with six columns of data and 1 header lines
+%   M = textToMat('myfile.csv', 1, 6)
 %-------------------------------------------------------------------------%
-    fileID = fopen(filename,'rt');
+    fileID = fopen(filename, 'rt');
     format = repmat('%f ', 1, numCols);
     data = textscan(fileID, format, 'delimiter', ',', ...
                     'HeaderLines', numHeaderLines);
+    M = zeros(length(data{1}), length(data));
     for i=1:length(data)
         M(:,i) = data{i};
     end   
